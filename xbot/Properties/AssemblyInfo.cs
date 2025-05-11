2f2a6a88f07be450979022daf410ccf5cfdcf30c
jaya <130752636+xjayax@users.noreply.github.com>
2025-05-11 20:18:40 +0200

    Add files via upload

---
+++ xbot/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("xBot")]
+[assembly: AssemblyDescription("Silkroad Online Bot")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("xBot")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("26fa42c7-c27b-4987-bef4-9c9e542db496")]
+
+[assembly: AssemblyVersion("0.5.2")]
+// 1. Mayor version
+// - Incompatibility between the most recent build
+// 2. Minor version 
+// - If a lot of changes has been made, has to include new features
+// 3. Revision :
+// - Any change done, bugfix or not
+[assembly: AssemblyFileVersion("0.5.2")]
